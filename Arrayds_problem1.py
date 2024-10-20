8517dc2401fa3fa0abdd0990d7ed362e5c89de36
Abhay Mishra <techabhay06@gmail.com>
2024-10-20 19:14:13 +0530

    Add new commit

--- Arrayds_problem1.py
+++ Arrayds_problem1.py
@@ -6,5 +6,18 @@ from array import *
 
 a1=array ('i', [23,24,12])
 
+# for loop 
+
 for i in a1:
     print(i)
+
+for i in range (3):
+    print(a1[i], end=" ")
+
+# while loop
+
+i=0
+
+while i<len(a1):
+    print(a1[i])
+    i+=1
