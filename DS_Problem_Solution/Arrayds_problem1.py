85a5500cc519f55556d275516349e09db14f8c09
Abhay Mishra <techabhay06@gmail.com>
2024-10-24 17:59:22 +0530

    Add new commit

--- DS_Problem_Solution/Arrayds_problem1.py
+++ DS_Problem_Solution/Arrayds_problem1.py
@@ -1,5 +1,6 @@
 
 # Problem number 1 - Array method 
+
 # Data Structure problem in Array 
 
 from array import * 
@@ -21,4 +22,17 @@ i=0
 while i<len(a1):
     print(a1[i])
     i+=1
+
+# here I solve DS array problem 
+
+# Question is two sum of number -
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []  # No solution found
     
