825697e5abfc17696853f38817ac646356dfce59
Abhay Mishra <techabhay06@gmail.com>
2024-10-25 16:28:25 +0530

    Add new commit

--- leetcode_dsproblem1.py
+++ leetcode_dsproblem1.py
@@ -1,5 +1,5 @@
 
-# we solve a leet problem here -
+#  Here solution of Python based on List two sum-
 
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
@@ -14,3 +14,14 @@ class Solution:
         
         return n <= 0
     
+# Here solution of Python based on Array tow sum-
+    
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return [] 
