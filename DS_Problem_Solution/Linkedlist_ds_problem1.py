3b6deae0384dc0860e96a48a670f5e018b09e156
Abhay Mishra <techabhay06@gmail.com>
2024-10-29 02:24:40 +0530

    Add new commit

--- DS_Problem_Solution/Linkedlist_ds_problem1.py
+++ DS_Problem_Solution/Linkedlist_ds_problem1.py
@@ -118,32 +118,31 @@ for x in mylist:
 
 # Here a Question base of Linked lis which is leetcode question-
 
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-# class Solution:
-#     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-#         dummyHead = ListNode(0)
-#         tail = dummyHead
-#         carry = 0
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummyHead = ListNode(0)
+        tail = dummyHead
+        carry = 0
 
-#         while l1 is not None or l2 is not None or carry != 0:
-#             digit1 = l1.val if l1 is not None else 0
-#             digit2 = l2.val if l2 is not None else 0
+        while l1 is not None or l2 is not None or carry != 0:
+            digit1 = l1.val if l1 is not None else 0
+            digit2 = l2.val if l2 is not None else 0
 
-#             sum = digit1 + digit2 + carry
-#             digit = sum % 10
-#             carry = sum // 10
+            sum = digit1 + digit2 + carry
+            digit = sum % 10
+            carry = sum // 10
 
-#             newNode = ListNode(digit)
-#             tail.next = newNode
-#             tail = tail.next
+            newNode = ListNode(digit)
+            tail.next = newNode
+            tail = tail.next
 
-#             l1 = l1.next if l1 is not None else None
-#             l2 = l2.next if l2 is not None else None
+            l1 = l1.next if l1 is not None else None
+            l2 = l2.next if l2 is not None else None
 
-#         result = dummyHead.next
-#         dummyHead.next = None
-#         return result
-    
+        result = dummyHead.next
+        dummyHead.next = None
+        return result
