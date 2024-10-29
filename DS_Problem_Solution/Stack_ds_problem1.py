5c6818de1517ffc6fb43f7ae5d69a715f7a4d25c
Abhay Mishra <techabhay06@gmail.com>
2024-10-30 01:36:55 +0530

    Add new commit

--- DS_Problem_Solution/Stack_ds_problem1.py
+++ DS_Problem_Solution/Stack_ds_problem1.py
@@ -1,1 +1,41 @@
 
+# Here stack question sloved
+
+class Stack:
+    def __init__(self):
+         self.item=[]
+        
+    def is_empty(self):
+         return len(self.item)==0
+    
+    def push(self, data):
+         self.item.append(data)
+
+    def pop(self):
+         if not self.is_empty():
+              return self.item.pop()
+         else:
+              raise IndexError("Stack is empty")
+
+    def peek(self):
+         if not self.is_empty():
+              return self.item[-1]
+         else:
+              raise IndexError("Stack is Empty")
+
+    def size(self):
+         return len(self.item) 
+
+s1= Stack()
+s1.push(4)
+s1.push(5)
+s1.push(6)
+print("Size of elemnt:", s1.size())
+print("top element:", s1.peek())
+print("Removed item:", s1.pop())
+print("top element:", s1.peek())
+print("Size of elemnt:", s1.size())
+print()
+
+
+    
