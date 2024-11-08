21b03b3f42346654235ce03d28f6e1acd872da72
Abhay Mishra <techabhay06@gmail.com>
2024-11-08 21:17:50 +0530

    Add new commit

--- DS_Problem_Solution/Stack_ds_problem2.py
+++ DS_Problem_Solution/Stack_ds_problem2.py
@@ -1,38 +1,65 @@
 
 # Here second problem solve of Stack Data Structure - 
 
+# class Stack(list):
+
+#     def is_empty(self):
+#          return len(self)==0
+    
+#     def push(self,data):
+#          self.append(data)
+
+#     def pop(self):
+#          if not self.is_empty():
+#               return super().pop()
+#          else:
+#               raise IndexError("List is Empty")
+         
+#     def peek(self):
+#          if not self.is_empty():
+#               return self[-1]
+#          else:
+#               raise IndexError("List is Empty")
+         
+#     def size(self):
+#          return len(self)
+    
+#     def insert(self, index, data):
+#          return AttributeError ("No attribute int this insert function")
+    
+# s1= Stack()
+# s1.push(1)
+# s1.push(2)
+# s1.push(3)
+# print("topvalue = ", s1.peek())
+# print(s1.pop())
+# print("topvalue = ", s1.peek())
+
+# second code try by my self again - 
+
 class Stack(list):
 
     def is_empty(self):
-         return len(self)==0
-    
+        return self()==0
     def push(self,data):
-         self.append(data)
-
+        return self.append(data)
     def pop(self):
-         if not self.is_empty():
-              return super().pop()
-         else:
-              raise IndexError("List is Empty")
-         
+        return super().pop()
     def peek(self):
-         if not self.is_empty():
-              return self[-1]
-         else:
-              raise IndexError("List is Empty")
-         
+        return self[-1]
     def size(self):
-         return len(self)
-    
+        return len(self)
     def insert(self, index, data):
-         return AttributeError ("No attribute int this insert function")
+        raise AttributeError("No elements")
     
 s1= Stack()
-s1.push(1)
 s1.push(2)
-s1.push(3)
-print("topvalue = ", s1.peek())
+s1.push(4)
+s1.push(6)
+print("top elements=", s1.peek())
+print(s1.size())
 print(s1.pop())
-print("topvalue = ", s1.peek())
+print("top elements=", s1.peek())
 
-    
+
+
