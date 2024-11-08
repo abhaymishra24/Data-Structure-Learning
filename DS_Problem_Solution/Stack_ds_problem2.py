d16d903f586f5e1d812c95bc94bfb298d783c58c
Abhay Mishra <techabhay06@gmail.com>
2024-11-08 21:06:30 +0530

    Add new commit

--- DS_Problem_Solution/Stack_ds_problem2.py
+++ DS_Problem_Solution/Stack_ds_problem2.py
@@ -1,29 +1,38 @@
 
 # Here second problem solve of Stack Data Structure - 
 
-class Stack:
-
-    def __init__(self):
-         self.item=[]
+class Stack(list):
 
     def is_empty(self):
-         return len(self.is_empty())==0
+         return len(self)==0
     
     def push(self,data):
-         self.item.append(data)
+         self.append(data)
 
     def pop(self):
          if not self.is_empty():
-              return self.item.pop()
+              return super().pop()
          else:
               raise IndexError("List is Empty")
          
     def peek(self):
          if not self.is_empty():
-              return self.item.peek()
+              return self[-1]
          else:
               raise IndexError("List is Empty")
          
     def size(self):
-         return len(self.item)
+         return len(self)
+    
+    def insert(self, index, data):
+         return AttributeError ("No attribute int this insert function")
+    
+s1= Stack()
+s1.push(1)
+s1.push(2)
+s1.push(3)
+print("topvalue = ", s1.peek())
+print(s1.pop())
+print("topvalue = ", s1.peek())
+
     
