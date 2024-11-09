4f4f99570135005f234f508b2c6940fa8e8fdd11
Abhay Mishra <techabhay06@gmail.com>
2024-11-09 13:22:43 +0530

    Add new commit

--- DS_Problem_Solution/Stack_ds_problem3.py
+++ DS_Problem_Solution/Stack_ds_problem3.py
@@ -1,6 +1,57 @@
 
 # Here third problem of Stack question -
 
+# class Node:
+
+#     def __init__(self, item=None, next=None):
+#         self.item=item
+#         self.next=next
+
+# class Stack:
+
+#     def __init__(self):
+#         self.start=None
+#         self.item_count=0
+
+#     def is_empty(self):
+#         return self.start==None
+    
+#     def push(self,data):
+#         n = Node(data, self.start)
+#         self.start=n
+#         self.item_count+=1 
+    
+#     def pop(self):
+#         if not self.is_empty():
+#             data= self.start.item
+#             self.start=self.start.next
+#             self.item_count-=1
+#             return data
+#         else:
+#             raise IndexError("Stack is empty")
+        
+#     def peek(self):
+#         if not self.is_empty():
+#             return self.start.item
+#         else:
+#             raise self.is_empty()
+        
+#     def size(self):
+#         return self.item_count     
+
+# s2=Stack()
+# s2.push(5)
+# s2.push(6)
+# s2.push(7)
+# print("total elemnts=", s2.size())
+# print("top value=", s2.peek())
+# print("remove top value=", s2.pop())
+# print("total elements again=", s2.size())
+# print("top value again=", s2.peek())
+
+
+# third code try by my self again -
+
 class Node:
 
     def __init__(self, item=None, next=None):
@@ -10,25 +61,22 @@ class Node:
 class Stack:
 
     def __init__(self):
-        self.start=None
-        self.item_count=0
-
+         self.start=None
+         self.item_count=0
     def is_empty(self):
-        return self.start==None
-    
-    def push(self,data):
-        n = Node(data, self.start)
+        return self.start.item==None
+    def push(self, data):
+        n= Node(data, self.start)
         self.start=n
-        self.item_count+=1 
-    
+        self.item_count+=1
+
     def pop(self):
         if not self.is_empty():
-            data= self.start.item
-            self.start=self.start.next
+            data=self.start.item
+            self.start=self.start.item
             self.item_count-=1
-            return data
         else:
-            raise IndexError("Stack is empty")
+            raise self.is_empty()
         
     def peek(self):
         if not self.is_empty():
@@ -37,15 +85,15 @@ class Stack:
             raise self.is_empty()
         
     def size(self):
-        return self.item_count     
-
+        return self.item_count
+    
 s2=Stack()
-s2.push(5)
-s2.push(6)
-s2.push(7)
-print("total elemnts=", s2.size())
-print("top value=", s2.peek())
-print("remove top value=", s2.pop())
-print("total elements again=", s2.size())
-print("top value again=", s2.peek())
+s2.push(8)
+s2.push(9)
+s2.push(0)
+print("len of elements is = ", s2.size())
+print("top elements of stack = ", s2.peek())
+print("delete a value from stack = ", s2.pop())
 
+            
+        
