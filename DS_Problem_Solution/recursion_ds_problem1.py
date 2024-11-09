4323f6cab0e1d63323ba0b218ef7e3a8b5baf1aa
Abhay Mishra <techabhay06@gmail.com>
2024-11-10 00:09:34 +0530

    Add new commit

--- DS_Problem_Solution/recursion_ds_problem1.py
+++ DS_Problem_Solution/recursion_ds_problem1.py
@@ -1,37 +1,54 @@
 
 # Here we solve basic recursion question
 
-def fn(n):
-    if n>0:
-        fn(n-1)
-        print(n, end=' ')
+# def fn(n):
+#     if n>0:
+#         fn(n-1)
+#         print(n, end=' ')
 
-def fnreverse(n):
-    if n>0:
-        print(n, end=' ')
-        fnreverse(n-1)
+# def fnreverse(n):
+#     if n>0:
+#         print(n, end=' ')
+#         fnreverse(n-1)
          
-def fnodd(n):
-    if n>0:
-        fnodd(n-1)
-        print(2*n-1, end=' ')
+# def fnodd(n):
+#     if n>0:
+#         fnodd(n-1)
+#         print(2*n-1, end=' ')
 
-def fneven(n):
-    if n>0:
-        fneven(n-1)
-        print(2*n, end=' ')
+# def fneven(n):
+#     if n>0:
+#         fneven(n-1)
+#         print(2*n, end=' ')
 
-def fnoddr(n):
-    if n>0:
-        print(2*n-1, end=' ')
-        fnoddr(n-1)
+# def fnoddr(n):
+#     if n>0:
+#         print(2*n-1, end=' ')
+#         fnoddr(n-1)
         
 
-def fnevenr(n):
-    if n>0:
-        print(2*n, end=' ')
-        fnevenr(n-1)
+# def fnevenr(n):
+#     if n>0:
+#         print(2*n, end=' ')
+#         fnevenr(n-1)
               
-fnevenr(10)
+# fnevenr(10)
 
+# Here we solve some basic mathmetics question by recursion-
 
+def sumn(n):
+    if n==0:
+        return 0
+    return n + sumn(n-1)
+
+def sumodd(n):
+    if n==1:
+        return 1
+    return 2*n-1 + sumodd(n-1)
+
+def sumeven(n):
+    if n==1:
+        return 1
+    return 2*n + sumeven(n-1)
+
+print(sumodd(10))
