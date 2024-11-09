fb68c6a8983413582e7a7c134987d80bdd34bf7b
Abhay Mishra <techabhay06@gmail.com>
2024-11-09 13:31:17 +0530

    Add new commit

--- DS_Problem_Solution/Stack_ds_problem3.py
+++ DS_Problem_Solution/Stack_ds_problem3.py
@@ -1,53 +1,53 @@
 
 # Here third problem of Stack question -
 
-# class Node:
+class Node:
 
-#     def __init__(self, item=None, next=None):
-#         self.item=item
-#         self.next=next
+    def __init__(self, item=None, next=None):
+        self.item=item
+        self.next=next
 
-# class Stack:
+class Stack:
 
-#     def __init__(self):
-#         self.start=None
-#         self.item_count=0
+    def __init__(self):
+        self.start=None
+        self.item_count=0
 
-#     def is_empty(self):
-#         return self.start==None
+    def is_empty(self):
+        return self.start==None
     
-#     def push(self,data):
-#         n = Node(data, self.start)
-#         self.start=n
-#         self.item_count+=1 
+    def push(self,data):
+        n = Node(data, self.start)
+        self.start=n
+        self.item_count+=1 
     
-#     def pop(self):
-#         if not self.is_empty():
-#             data= self.start.item
-#             self.start=self.start.next
-#             self.item_count-=1
-#             return data
-#         else:
-#             raise IndexError("Stack is empty")
+    def pop(self):
+        if not self.is_empty():
+            data= self.start.item
+            self.start=self.start.next
+            self.item_count-=1
+            return data
+        else:
+            raise IndexError("Stack is empty")
         
-#     def peek(self):
-#         if not self.is_empty():
-#             return self.start.item
-#         else:
-#             raise self.is_empty()
+    def peek(self):
+        if not self.is_empty():
+            return self.start.item
+        else:
+            raise self.is_empty()
         
-#     def size(self):
-#         return self.item_count     
+    def size(self):
+        return self.item_count     
 
-# s2=Stack()
-# s2.push(5)
-# s2.push(6)
-# s2.push(7)
-# print("total elemnts=", s2.size())
-# print("top value=", s2.peek())
-# print("remove top value=", s2.pop())
-# print("total elements again=", s2.size())
-# print("top value again=", s2.peek())
+s2=Stack()
+s2.push(5)
+s2.push(6)
+s2.push(7)
+print("total elemnts=", s2.size())
+print("top value=", s2.peek())
+print("remove top value=", s2.pop())
+print("total elements again=", s2.size())
+print("top value again=", s2.peek())
 
 
 # third code try by my self again -
@@ -98,5 +98,3 @@ print("top elements of stack = ", s2.peek())
 print("delete a value from stack = ", s2.pop())
 print("top value again is = ",s2.peek())
 print("size of elements is = ", s2.size())
-            
-        
