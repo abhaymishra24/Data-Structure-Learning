af5726a90dad637e73f2a2db93b9e685011a7eef
Abhay Mishra <techabhay06@gmail.com>
2024-11-11 11:24:46 +0530

    Add new commit

--- DS_Problem_Solution/recursion_ds_problem2.py
+++ DS_Problem_Solution/recursion_ds_problem2.py
@@ -1,10 +1,28 @@
 
 # Here we solve two sum of given number - 
 
-def sum(a,b):
+# def sum(a,b):
 
-    print(a+b)
+#     print(a+b)
 
-sum(5,6)
+# sum(5,6)
+
+def sum(nums, target):
+    
+    n=len(nums)
+
+    for i in range(n-1):
+
+        for j in range(i+1, n):
+
+            if nums[i] + nums[j] == target:
+                return [i,j]
+             
+    return[] 
+
+nums=[4,6,7,8]
+target=10
+result=(sum(nums,target))
+print(result)
 
     
