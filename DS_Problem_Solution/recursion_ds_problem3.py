4e856cc9657bfea4b309998c80738cc26d5e9b09
Abhay Mishra <techabhay06@gmail.com>
2024-11-12 01:51:54 +0530

    Add new commit

--- DS_Problem_Solution/recursion_ds_problem3.py
+++ DS_Problem_Solution/recursion_ds_problem3.py
@@ -2,35 +2,63 @@
 # here we solve longest substring - 
 
 
-def longestUniqueSubstr(s):
-    n = len(s)
+# def longestUniqueSubstr(s):
+#     n = len(s)
+#     res = 0
+
+#     for i in range(n):
+
+#         # Initializing all characters as not visited
+#         visited = [False] * 256
+
+#         for j in range(i, n):
+
+#             # If current character is visited
+#             # Break the loop
+#             if visited[ord(s[j])] == True:
+#                 break
+
+#             # Else update the result if this window is larger,
+#             # and mark current character as visited.
+#             else:
+#                 res = max(res, j - i + 1)
+#                 visited[ord(s[j])] = True
+
+#     return res
+
+# if __name__ == "__main__":
+#     s = "geeksforgeeks"
+#     print(longestUniqueSubstr(s))
+
+# Here we try once again this code by itself - 3
+
+def longss(s):
+    n=len(s)
     res = 0
 
     for i in range(n):
 
-        # Initializing all characters as not visited
-        visited = [False] * 256
+        visited=[False]*256
 
-        for j in range(i, n):
+        for j in range(i,n):
 
-            # If current character is visited
-            # Break the loop
-            if visited[ord(s[j])] == True:
+            if visited[(ord(s[j]))]==True:
                 break
 
-            # Else update the result if this window is larger,
-            # and mark current character as visited.
-            else:
-                res = max(res, j - i + 1)
-                visited[ord(s[j])] = True
+            else: 
+                res= max(res, j-i+1)
+                visited[(ord(s[j]))]= True
 
     return res
 
-if __name__ == "__main__":
-    s = "geeksforgeeks"
-    print(longestUniqueSubstr(s))
+if __name__=="main":
+    s= "geeksforgeeks"   
+    print(longss(s))         
 
-# Here we try once again this code by itself - 
+
+
+
+         
 
 
 
