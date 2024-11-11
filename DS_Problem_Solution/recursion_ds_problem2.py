e9e9c20d794a664490bb7c7ace2fcd2325477951
Abhay Mishra <techabhay06@gmail.com>
2024-11-12 00:36:32 +0530

    Add new commit

--- DS_Problem_Solution/recursion_ds_problem2.py
+++ DS_Problem_Solution/recursion_ds_problem2.py
@@ -58,18 +58,8 @@ def max_profit(prices):
 prices = [7, 1, 5, 3, 6, 4]
 print("Maximum Profit:", max_profit(prices))
 
-# class Solution:
-#     def maxProfit(self, prices: List[int]) -> int:
-#         profit = 0
-        
-#         for i in range(1, len(prices)):
-#             if prices[i] > prices[i-1]:
-#                 profit += prices[i] - prices[i-1]
-        
-#         return profit
 
 # first and last accurence sorted 
-
 
 def findFirstAndLast(arr, n, x):
     first = -1
