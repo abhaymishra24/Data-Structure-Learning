1272bfde6bdf24787659d1dc9bff9d66d701afc9
Abhay Mishra <techabhay06@gmail.com>
2024-11-12 00:43:52 +0530

    Add new commit

--- DS_Problem_Solution/recursion_ds_problem1.py
+++ DS_Problem_Solution/recursion_ds_problem1.py
@@ -119,20 +119,3 @@ sum = 22                                                    # give the value of 
 
 number3sum(arr, sum)                                        # call the function for answer 
 
-
-# Here the code of longest substring -
-
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        left = max_length = 0
-        char_set = set()
-        
-        for right in range(len(s)):
-            while s[right] in char_set:
-                char_set.remove(s[left])
-                left += 1
-
-            char_set.add(s[right])
-            max_length = max(max_length, right - left + 1)
-        
-        return max_length    
