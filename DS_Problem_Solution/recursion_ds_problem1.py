e905db8e4c1666735735d8e3c2867354a2a3bc6f
Abhay Mishra <techabhay06@gmail.com>
2024-11-27 16:08:37 +0530

    Add new commit

--- DS_Problem_Solution/recursion_ds_problem1.py
+++ DS_Problem_Solution/recursion_ds_problem1.py
@@ -62,16 +62,3 @@ def factSq(n):
     return n*n + factSq(n-1)
 
 print("print the answer = ",factSq(5))
-
-# profit sell stock price - leetcode-
-
-# class Solution:
-#     def maxProfit(self, prices: List[int]) -> int:
-#         profit = 0
-        
-#         for i in range(1, len(prices)):
-#             if prices[i] > prices[i-1]:
-#                 profit += prices[i] - prices[i-1]
-        
-#         return profit
-
