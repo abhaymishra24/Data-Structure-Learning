fa2ed209cfa63d3a2f18b0607fb25e38f459385b
Abhay Mishra <techabhay06@gmail.com>
2024-11-28 12:39:26 +0530

    Add new commit

--- leetcode_ds_problem2.java
+++ leetcode_ds_problem2.java
@@ -3,15 +3,51 @@
 
 // first leet question - 
 
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         for (int i = 0; i < nums.length; i++) {
+//             for (int j = i + 1; j < nums.length; j++) {
+//                 if (nums[i] + nums[j] == target) {
+//                     return new int[] {i, j};
+//                 }
+//             }
+//         }
+//         return new int[] {};
+//     }
+// }
+
+// threeSum Question and solution (leetcode, medium)
+
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] {i, j};
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
+
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
+                    j++;
+                } else {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+
+                    while (nums[j] == nums[j-1] && j < k) {
+                        j++;
+                    }
                 }
             }
         }
-        return new int[] {};
+        return res;        
     }
 }
