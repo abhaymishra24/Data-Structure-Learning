255b760e859853ce05d8d5eeaaf5624c1812e58b
Abhay Mishra <techabhay06@gmail.com>
2024-11-28 11:50:49 +0530

    Add new commit

--- Oops_practice_question3.py
+++ Oops_practice_question3.py
@@ -1,4 +1,15 @@
 
+# First Question -
+
 name = 'Bob'
 age = 25
 print('{0} is {1} years old and {0} likes Python.'.format(name, age))
+
+
+# Second Question -
+
+import re
+text = 'The quick brown fox jumps over the lazy dog'
+pattern = r'\b\w{5}\b'
+matches = re.findall(pattern, text)
+print(len(matches))
