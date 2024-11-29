7de219a85dc11536379ce3dcf6066eb16f1177f3
Abhay Mishra <techabhay06@gmail.com>
2024-11-29 22:52:17 +0530

    Add new commit

--- Oops_practice_question4.py
+++ Oops_practice_question4.py
@@ -15,3 +15,21 @@ def tsum(num, target):
 num = [5,6,3,2,4,5]
 target = 10
 tsum(num, target)
+
+
+def thsum(num,sum):
+         
+        n = len(num)
+        
+        for i in range(n-2):
+            for j in range(i+1,n-1):
+                for k in range(j+1, n):
+                    if num[i] + num[j] + num[k] == sum:
+                        print(num[i],num[j],num[k])
+                        return True
+        return False
+    
+num = [2,3,4,5,6,7,8,9,0]
+sum = 10
+thsum(num, sum)
+
