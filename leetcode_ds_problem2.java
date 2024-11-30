567c4b82a6f6d7ba2147c1511828e54e4feff424
Abhay Mishra <techabhay06@gmail.com>
2024-12-01 00:42:11 +0530

    Add new commit

--- leetcode_ds_problem2.java
+++ leetcode_ds_problem2.java
@@ -20,36 +20,36 @@ class Solution {
 
 // threeSum Question and solution (leetcode, medium)
 
-// class Solution {
-//     public List<List<Integer>> threeSum(int[] nums) {
-//         List<List<Integer>> res = new ArrayList<>();
-//         Arrays.sort(nums);
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
 
-//         for (int i = 0; i < nums.length; i++) {
-//             if (i > 0 && nums[i] == nums[i-1]) {
-//                 continue;
-//             }
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
             
-//             int j = i + 1;
-//             int k = nums.length - 1;
+            int j = i + 1;
+            int k = nums.length - 1;
 
-//             while (j < k) {
-//                 int total = nums[i] + nums[j] + nums[k];
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
 
-//                 if (total > 0) {
-//                     k--;
-//                 } else if (total < 0) {
-//                     j++;
-//                 } else {
-//                     res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-//                     j++;
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
+                    j++;
+                } else {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
 
-//                     while (nums[j] == nums[j-1] && j < k) {
-//                         j++;
-//                     }
-//                 }
-//             }
-//         }
-//         return res;        
-//     }
-// }
+                    while (nums[j] == nums[j-1] && j < k) {
+                        j++;
+                    }
+                }
+            }
+        }
+        return res;        
+    }
+}
