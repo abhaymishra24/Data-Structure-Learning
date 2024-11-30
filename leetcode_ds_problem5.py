86aeb18f9867d26d3b0b214de8c5863f5d07fb9d
Abhay Mishra <techabhay06@gmail.com>
2024-11-30 16:34:06 +0530

    Add new commit

--- leetcode_ds_problem5.py
+++ leetcode_ds_problem5.py
@@ -9,3 +9,26 @@ class Solution(object):
   
   
 # Find the Index of the First Occurrence in a String  (leetcode, easy)
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        # makes sure we don't iterate through a substring that is shorter than needle
+        for i in range(len(haystack) - len(needle) + 1):
+            # check if any substring of haystack with the same length as needle is equal to needle
+            if haystack[i : i+len(needle)] == needle:
+                # if yes, we return the first index of that substring
+                return i
+        # if we exit the loop, return -1        
+        return -1
+
+
+
+
+
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
