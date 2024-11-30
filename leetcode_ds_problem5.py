e6356a7ab11cfff45f0429ca83c65b9aa57376b2
Abhay Mishra <techabhay06@gmail.com>
2024-11-30 16:34:38 +0530

    Add new commit

--- leetcode_ds_problem5.py
+++ leetcode_ds_problem5.py
@@ -22,13 +22,10 @@ class Solution(object):
         return -1
 
 
-
-
-
-
 class Solution(object):
     def strStr(self, haystack, needle):
         for i in range(len(haystack) - len(needle) + 1):
             if haystack[i:i+len(needle)] == needle:
                 return i
         return -1
+    
