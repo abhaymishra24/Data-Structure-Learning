d5ff7d441de1328e29c58fd93beff321c1b7d1ec
Abhay Mishra <techabhay06@gmail.com>
2024-12-01 17:17:39 +0530

    Add new commit

--- leetcode_ds_problem4.java
+++ leetcode_ds_problem4.java
@@ -43,6 +43,8 @@ class Solution {
 //             n-=1
 //             end_idx-=1
 
+// codechef probelm with Explanation (basic question of Java)//
+
 // double a = 0.1;
 // double b = 0.2;
 // System.out.println(a + b == 0.3);
@@ -54,5 +56,4 @@ class Solution {
 // Explanation:
 // The output is false. This is due to the way floating-point numbers are represented in binary. Most decimal fractions cannot be represented exactly as binary fractions, leading to small rounding errors.
 // In this case, 0.1 + 0.2 is slightly larger than 0.3 when represented in binary floating-point. For precise decimal calculations, it's recommended to use BigDecimal instead of double.
-            
             
