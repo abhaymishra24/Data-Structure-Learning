05df0d85e7d96aa17abf75b3b0f5d0c29eaa92a9
Abhay Mishra <techabhay06@gmail.com>
2024-12-02 22:27:48 +0530

    Add new commit

--- Oops_practice_question4.py
+++ Oops_practice_question4.py
@@ -61,7 +61,15 @@ fsum(num, tar)
 def anb(arr, t):
     
     n = len(arr)
-    
+    for i in range(n-2):
+        for j in range(i+1, n-1):
+            for k in range(j+1, n):
+                if arr[i] + arr[j] + arr[k] == t:
+                    print(f"here product number : {arr[i]},{arr[j]},{arr[k]}")
+                    
+    return False
+
+
 
                         
                      
