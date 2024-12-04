36c75bdfa97d60369c1f777aa832bd1f78e2f4d1
Abhay Mishra <techabhay06@gmail.com>
2024-12-04 20:13:26 +0530

    Add new commit

--- DS_Problem_Solution/recursion_ds_problem5.py
+++ DS_Problem_Solution/recursion_ds_problem5.py
@@ -153,4 +153,14 @@ Dsum(arr, sum)
 
 def sut(num, t):
     
+    n = len(num)
+    
+    for i in range(n-2):
+        for j in range(i+1, n-1):
+            for k in range(j+1, n):
+                if num[i] + num [j] + num [k] == t:
+                    print("number is here of t : {num[i]},{num[j]},{num[k]}. solve it.")
+                    return True
+    return False
+    
     
