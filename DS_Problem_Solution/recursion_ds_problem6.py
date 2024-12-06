b6211af69f906ab27bc6747841350de720226f6b
Abhay Mishra <techabhay06@gmail.com>
2024-12-06 15:15:00 +0530

    Add new commit

--- DS_Problem_Solution/recursion_ds_problem6.py
+++ DS_Problem_Solution/recursion_ds_problem6.py
@@ -51,3 +51,20 @@ print(maxProfit(prices))  # Output: 5
 
 # here we solve without comments -
 
+def maxProfit(prices):
+    min_price = float('inf')   
+    max_profit = 0 
+    
+    for price in prices:
+        if price < min_price:
+            min_price = price
+            
+        current_profit = price - min_price
+        
+        if current_profit > max_profit:
+            max_profit = current_profit
+
+    return max_profit
+
+prices = [7, 1, 5, 3, 6, 4]
+print(maxProfit(prices))   
