5bf2f2e38b08484216c910a8d035980d89db2f4a
Abhay Mishra <techabhay06@gmail.com>
2024-12-06 15:11:21 +0530

    Add new commit

--- DS_Problem_Solution/recursion_ds_problem6.py
+++ DS_Problem_Solution/recursion_ds_problem6.py
@@ -1,27 +1,29 @@
 
 # Best Time to Buy and Sell Stock - leetcode (easy)
 
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        price = float('inf')
-        profit = 0
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         price = float('inf')
+#         profit = 0
 
-        for i in prices:
-            price = min (i, price)
-            profit = max (profit, i - price)
+#         for i in prices:
+#             price = min (i, price)
+#             profit = max (profit, i - price)
              
-        return profit
+#         return profit
     
     
 # Best Time to Buy and Sell Stock II - leetcode (medium)
 
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         profit = 0
         
-        for i in range(1, len(prices)):
-            if prices[i] > prices[i-1]:
-                profit += prices[i] - prices[i-1]
+#         for i in range(1, len(prices)):
+#             if prices[i] > prices[i-1]:
+#                 profit += prices[i] - prices[i-1]
         
-        return profit
+#         return profit
 
+
+ 
