8ef4046a120829ee9f8a88e1a2b216c58dd65b71
Abhay Mishra <techabhay06@gmail.com>
2024-12-07 12:44:57 +0530

    Add new commit

---
+++ leetcode_ds_problem6.py
@@ -0,0 +1,13 @@
+
+def second_largest_sort(lst):
+    lst.sort()  # Sort the list in ascending order
+    return lst[-2]  # Return the second last element
+
+# Input from user
+li = []
+n = int(input("Enter size of list: "))
+for i in range(n):
+    e = int(input("Enter element of list: "))
+    li.append(e)
+
+print("Second largest in", li, "is", second_largest_sort(li))
