20fbf8df23731964cd62e1622168be970d3904a1
Abhay Mishra <techabhay06@gmail.com>
2024-12-07 12:48:12 +0530

    Add new commit

--- leetcode_ds_problem6.py
+++ leetcode_ds_problem6.py
@@ -20,3 +20,16 @@ print("Second largest in", li, "is", second_largest_sort(li))
 
 # solve this question with the (Removing the Maximum Element)
 
+def second_largest_remove_max(lst):
+    max_value = max(lst)  # Find the maximum value
+    lst.remove(max_value)  # Remove it from the list
+    return max(lst)  # Return the new maximum, which is the second largest
+
+# Input from user
+li = []
+n = int(input("Enter size of list: "))
+for i in range(n):
+    e = int(input("Enter element of list: "))
+    li.append(e)
+
+print("Second largest in", li, "is", second_largest_remove_max(li))
