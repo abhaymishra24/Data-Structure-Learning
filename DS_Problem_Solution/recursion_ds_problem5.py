c5aa08a4ef050e1987046f0c203a9c467a666148
Abhay Mishra <techabhay06@gmail.com>
2024-12-09 23:50:58 +0530

    Add new commit

--- DS_Problem_Solution/recursion_ds_problem5.py
+++ DS_Problem_Solution/recursion_ds_problem5.py
@@ -222,3 +222,14 @@ def twonumber(a, ad):
 a = [3,4,5,6,7,8]
 ad = 7
 twonumber(a, ad)
+
+# write this code once again - 
+
+def add(num, tar):
+    
+    n = len(num)
+    
+    for i in range(n-2):
+        for j in range(i+1, n-1):
+            for k in range(j+1, n):
+                if
