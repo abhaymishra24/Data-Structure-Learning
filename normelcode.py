987761abb0d5a4ba9511e2466d3497b849542c77
Abhay Mishra <techabhay06@gmail.com>
2024-12-09 20:39:19 +0530

    Add new commit

--- normelcode.py
+++ normelcode.py
@@ -1,2 +1,10 @@
 
 # here we solve normel code -
+
+def second_largest_sorting(numbers):
+    numbers.sort(reverse=True)
+    return numbers[1]
+
+numbers = [12, 45, 2, 41, 31, 10, 45, 65, 78]
+second_largest = second_largest_sorting(numbers)
+print("Second largest number:", second_largest)
