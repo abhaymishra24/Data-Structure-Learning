3cec77ed4f62f42bbdf0f39ba5fb271b7f40b8ad
Abhay Mishra <techabhay06@gmail.com>
2024-12-09 23:52:36 +0530

    Add new commit

--- DS_Problem_Solution/recursion_ds_problem5.py
+++ DS_Problem_Solution/recursion_ds_problem5.py
@@ -232,4 +232,9 @@ def add(num, tar):
     for i in range(n-2):
         for j in range(i+1, n-1):
             for k in range(j+1, n):
-                if
+                if num[i] + num[j] + num[k] == tar:
+                    print("the number is for tar here : {num[i]}, {num[j]}, {num[k]}. Thankyou")
+                    return True
+                
+    return True
+
