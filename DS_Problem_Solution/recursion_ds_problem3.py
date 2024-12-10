77bcee3417e03f139c75aebc7bed3961ce1249a3
Abhay Mishra <techabhay06@gmail.com>
2024-12-10 13:59:26 +0530

    Add new commit

--- DS_Problem_Solution/recursion_ds_problem3.py
+++ DS_Problem_Solution/recursion_ds_problem3.py
@@ -145,4 +145,26 @@ def longeststring(a):
     return res
 
 a = "abhaybhiaabhay"
-print(longeststring(a))               
+print(longeststring(a))          
+
+# https://leetcode.com/problems/find-longest-special-substring-that-occurs-thrice-i/description/?envType=daily-question&envId=2024-12-10
+
+class Solution(object):
+    def maximumLength(self, s):
+
+        if len(set(s))==len(s):
+            return -1
+        for i in range(len(s)-3,-1,-1):
+            d = {}
+            count = 0
+            for j in range(i,len(s)):
+                if len(set(s[count:j+1]))==1:
+                    if s[count:j+1] not in d:
+                        d[s[count:j+1]]=1
+                    else:
+                        d[s[count:j+1]]+=1
+                        if d[s[count:j+1]]==3:
+                            return len(s[count:j+1])
+                count+=1
+        return -1
+          
