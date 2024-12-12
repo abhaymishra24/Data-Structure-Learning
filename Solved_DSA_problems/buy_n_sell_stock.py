4d13293338346ab096e1da1fba2adc5b57067a93
Abhay Mishra <techabhay06@gmail.com>
2024-12-12 23:35:12 +0530

    Add new commit

--- Solved_DSA_problems/buy_n_sell_stock.py
+++ Solved_DSA_problems/buy_n_sell_stock.py
@@ -109,3 +109,25 @@ def maxProfit(prices):
 
 prices = [7, 1, 5, 3, 6, 4]
 print(maxProfit(prices))   
+
+
+# add new once code -
+
+def maxProfit(prices):
+    min_price = float('inf')   
+    max_profit = 0 
+    
+    for price in prices:
+        if price < min_price:
+            min_price = price
+            
+        current_profit = price - min_price
+        
+        if current_profit > max_profit:
+            max_profit = current_profit
+
+    return max_profit
+
+prices = [7, 1, 5, 3, 6, 4]
+print(maxProfit(prices))   
+
