cf32518d214494c40b53f12efbad0ea18c54148c
Abhay Mishra <techabhay06@gmail.com>
2024-12-15 22:43:32 +0530

    Add new commit

--- Solved_DSA_problems/second_largest_value.py
+++ Solved_DSA_problems/second_largest_value.py
@@ -142,3 +142,13 @@ second_largest = second_largest_sorting(numbers)
 
 # Print the result
 print("Second largest number:", second_largest)
+
+# solve once again - 
+
+def second_largest_sorting(numbers):
+    numbers.sort(reverse=True)
+    return numbers[1]
+
+numbers = [12, 45, 2, 41, 31, 10, 45, 65, 78]
+second_largest = second_largest_sorting(numbers)
+print("Second largest number:", second_largest)
