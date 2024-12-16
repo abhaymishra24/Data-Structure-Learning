e5c23b3ca2f245371c39d180976966e52fab6444
Abhay Mishra <techabhay06@gmail.com>
2024-12-16 14:44:49 +0530

    Add new commit

--- Solved_DSA_problems/remove_duplicates_array.py
+++ Solved_DSA_problems/remove_duplicates_array.py
@@ -1,36 +1,36 @@
 
-# Remove Duplicates from Sorted Array - leetcode (easy) 
+# # Remove Duplicates from Sorted Array - leetcode (easy) 
 
-class Solution(object):
-    def removeDuplicates(self, nums):
-        if not nums:
-            return 0
+# class Solution(object):
+#     def removeDuplicates(self, nums):
+#         if not nums:
+#             return 0
         
-        k = 1  # Initialize the count of unique elements to 1
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i - 1]:
-                nums[k] = nums[i]  # Overwrite the next unique element
-                k += 1
+#         k = 1  # Initialize the count of unique elements to 1
+#         for i in range(1, len(nums)):
+#             if nums[i] != nums[i - 1]:
+#                 nums[k] = nums[i]  # Overwrite the next unique element
+#                 k += 1
         
-        return k
+#         return k
     
-# Remove Duplicates from Sorted Array II - leetcode (medium)
+# # Remove Duplicates from Sorted Array II - leetcode (medium)
 
-# class Solution:
-def removeDuplicates(arr, nums):
+# # class Solution:
+# def removeDuplicates(arr, nums):
     
-    k = 2
+#     k = 2
 
-    for i in range(2, len(nums)):
-        if nums[i] != nums[k - 2]:
-            nums[k] = nums[i]
-            k += 1 
+#     for i in range(2, len(nums)):
+#         if nums[i] != nums[k - 2]:
+#             nums[k] = nums[i]
+#             k += 1 
 
-    return k
+#     return k
 
-arr = [8,8,9,3,4,5]
-nums = len(arr)
-print(removeDuplicates(arr,nums))
+# arr = [8,8,9,3,4,5]
+# nums = len(arr)
+# print(removeDuplicates(arr,nums))
 
 # solve once again this problem - 
 
@@ -45,6 +45,8 @@ def removedubli(arr, num):
             
     return n
 
-
+arr = [4,5,6,7,7,8,9]
+num = len(arr)
+print(removedubli(arr, num))
     
     
