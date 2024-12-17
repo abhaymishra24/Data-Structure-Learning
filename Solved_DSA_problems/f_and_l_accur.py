2774d5174b3bb18c88bfb5f0bef25a11129f48bc
Abhay Mishra <techabhay06@gmail.com>
2024-12-18 02:00:33 +0530

    Add new commit

--- Solved_DSA_problems/f_and_l_accur.py
+++ Solved_DSA_problems/f_and_l_accur.py
@@ -204,7 +204,6 @@ def first_and_last_occurrence(arr, target):
     last_occurrence = len(arr) - 1 - arr[::-1].index(target)
     return first_occurrence, last_occurrence
 
-# Example usage:
 arr = [1, 2, 3, 4, 2, 5, 6, 2, 7, 8]
 target = 2
 first, last = first_and_last_occurrence(arr, target)
