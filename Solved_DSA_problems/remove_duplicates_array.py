7664c682cf7065d6d0bdabc26309711f5154f9e5
Abhay Mishra <techabhay06@gmail.com>
2024-12-18 00:55:11 +0530

    Add enw commit

--- Solved_DSA_problems/remove_duplicates_array.py
+++ Solved_DSA_problems/remove_duplicates_array.py
@@ -70,3 +70,12 @@ def dubli(arr):
 arr = [3,4,5,6,7,8,8,8,9,4,4]
 r = dubli(arr)
 print(r)
+
+# solve this problem once again -
+
+def dublicates(arr):
+    return list(set(arr))
+
+arr = [8,6,7,7,8,9,9,0,0]
+rr = dublicates(arr)
+print(rr)
