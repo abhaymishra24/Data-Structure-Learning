535b4b99c1105b3eee603036e15de1e781b8e98d
Abhay Mishra <techabhay06@gmail.com>
2025-01-02 20:59:20 +0530

    Add new commit

--- leetcode_ds_problem5.py
+++ leetcode_ds_problem5.py
@@ -33,5 +33,20 @@
 
 # write code here for leet code - 
 
-def count_string_num(words, queris):
+def count_string_num(words, queries):
+    
+        n = len(words)
+        Prefix = [0] * (n + 1)
+        vowels = {'a', 'e', 'i', 'o', 'u'}
+
+        for i in range(n):
+            Prefix[i + 1] = Prefix[i]
+            if words[i][0] in vowels and words[i][-1] in vowels:
+                Prefix[i + 1] += 1
+
+        ANS = []
+        for query in queries:
+            ANS.append(Prefix[query[1] + 1] - Prefix[query[0]])
+
+        return ANS
     
