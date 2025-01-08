647563f5e9bbe21d265a7f23c305862852512566
Abhay Mishra <techabhay06@gmail.com>
2025-01-08 18:18:53 +0530

    Add new commit

--- leetcode_ds_problem7.py
+++ leetcode_ds_problem7.py
@@ -4,12 +4,11 @@ def movezero(nums):
     
     n = len(nums)
     
-    for i in range(0, n-1):
+    for i in range(n-1):
         for j in range(i+1, n):
-            if nums[j] == 0:
+            if nums[i] > nums[j]:
                 print(nums[i])
-                continue
-            if nums[i] == 0:
+            else:
                 print(nums[j])
                 
     return nums
