3d72ca71e129f1a8a1479a57297e32942c0d0de9
Abhay Mishra <techabhay06@gmail.com>
2025-01-09 21:03:02 +0530

    Add new commit

--- leetcode_ds_problem7.py
+++ leetcode_ds_problem7.py
@@ -1,40 +1,51 @@
 
 
-# solve with pointer - 
+# # solve with pointer - 
 
-def movezero(nums):
+# def movezero(nums):
     
-    last_zero = 0
+#     last_zero = 0
     
-    for i in range(len(nums)):
+#     for i in range(len(nums)):
         
-        if nums[i] != 0:
-            nums[last_zero] = nums[i]
-            last_zero += 1
+#         if nums[i] != 0:
+#             nums[last_zero] = nums[i]
+#             last_zero += 1
             
-    for i in range(last_zero, len(nums)):
-        nums[i] = 0
+#     for i in range(last_zero, len(nums)):
+#         nums[i] = 0
         
-    return nums
+#     return nums
 
-nums = [0,1,0,3,12]
-r = movezero(nums)
-print(r)
+# nums = [0,1,0,3,12]
+# r = movezero(nums)
+# print(r)
 
-# same question with different method - 
+# # same question with different method - 
 
-# Using List Comprehension
-# This method creates a new list with non-zero elements followed by the appropriate number of zeros.
+# # Using List Comprehension
+# # This method creates a new list with non-zero elements followed by the appropriate number of zeros.
 
-def movezero(nums):
-    # Create a new list with non-zero elements followed by zeros
-    non_zero_elements = [num for num in nums if num != 0]
-    zero_count = len(nums) - len(non_zero_elements)
+# def movezero(nums):
+#     # Create a new list with non-zero elements followed by zeros
+#     non_zero_elements = [num for num in nums if num != 0]
+#     zero_count = len(nums) - len(non_zero_elements)
     
-    # Extend the list with zeros
-    return non_zero_elements + [0] * zero_count
+#     # Extend the list with zeros
+#     return non_zero_elements + [0] * zero_count
 
-# Example usage
-nums = [0, 1, 0, 3, 12]
-r = movezero(nums)
-print(r)  # Output: [1, 3, 12, 0, 0]
+# # Example usage
+# nums = [0, 1, 0, 3, 12]
+# r = movezero(nums)
+# print(r)  # Output: [1, 3, 12, 0, 0]
+
+# solve leetcode question - 
+
+def count_pref_number(self, words, prefix):
+    
+    prefix = 0
+
+    for i in range(len(words)):
+        if words[i] == prefix:
+            print()
+        
