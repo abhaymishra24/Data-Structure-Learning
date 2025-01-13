5abee06b52b4ebdcde9329785dbca5df86f17d35
Abhay Mishra <techabhay06@gmail.com>
2025-01-13 21:30:51 +0530

    Add new commit

--- leetcode_ds_problem8.py
+++ leetcode_ds_problem8.py
@@ -1,6 +1,8 @@
 
 # find out minimum length of string after operation - 
 
+# Approach 1: Using Hash Map
+
 class Solution(object):
     def minimumLength(self, s):
         # Step 1: Count the frequency of each character in the string
@@ -19,3 +21,22 @@ class Solution(object):
         # Step 3: Return the minimum length after deletions
         return len(s) - delete_count
         
+
+# Approach 1: Using Hash Map
+
+def minimumLength(self, s: str) -> int:
+        arr = [0] * 26
+        
+        # Counting the char
+        for ch in s:
+            arr[ord(ch) - ord('a')] += 1
+        
+        # Applying the rules
+        for i in range(26):
+            while arr[i] >= 3:
+                arr[i] -= 2
+        
+        # Counting the length of final string
+        length = sum(arr)
+        
+        return length
