6ae6f619ce37cc6bca3a255b78400a15d8e3f71a
Abhay Mishra <techabhay06@gmail.com>
2025-01-16 23:08:11 +0530

    Add new commit

--- Solved_DSA_problems/Entry_check.py
+++ Solved_DSA_problems/Entry_check.py
@@ -9,4 +9,15 @@ def entry_check(age):
             print("no")
         
 x = int(input("Enter your age: "))
-entry_check(x)
+entry_check(x) 
+
+def identity(nation):
+    
+    if nation == 'Indian':
+        print("Yes, entry")
+        
+    else:
+        print("No")
+        
+n = input("Enter your Nation: ")
+identity(n)
