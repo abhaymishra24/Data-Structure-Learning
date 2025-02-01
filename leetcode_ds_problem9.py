f6b2654c7e8bfda1470a8f9f7122efc1e66ee2c2
Abhay Mishra <techabhay06@gmail.com>
2025-02-01 22:04:22 +0530

    Add new commit

--- leetcode_ds_problem9.py
+++ leetcode_ds_problem9.py
@@ -3,10 +3,12 @@
 
 # An array is considered special if every pair of its adjacent elements contains two numbers with different parity.
 # You are given an array of integers nums. Return true if nums is a special array, otherwise, return false.
- # this is answer - 
+
+# this is answer - 
 class Solution(object):
     def isArraySpecial(self, nums):
         for i in range(len(nums) - 1):
             if (nums[i] % 2) == (nums[i + 1] % 2):
                 return False  # If they have the same parity, return False
         return True
+    
