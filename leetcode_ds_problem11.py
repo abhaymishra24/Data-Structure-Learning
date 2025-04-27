35bf7d98de6f2fb608ab465979a39cfd373b1d88
Abhay Mishra <techabhay06@gmail.com>
2025-04-27 09:21:12 +0530

    Add new commit

---
+++ leetcode_ds_problem11.py
@@ -0,0 +1,9 @@
+
+# here we solve leed code problem - 
+
+# Given an integer array nums, return the number of subarrays of length 3 such that the sum of
+# the first and third numbers equals exactly half of the second number.
+
+
+
+ 
