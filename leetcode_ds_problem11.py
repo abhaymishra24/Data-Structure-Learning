c838fb1e248c28b3d3a504ff4b6558abd6537061
Abhay Mishra <techabhay06@gmail.com>
2025-04-27 09:21:57 +0530

    Add new commit

--- leetcode_ds_problem11.py
+++ leetcode_ds_problem11.py
@@ -4,6 +4,15 @@
 # Given an integer array nums, return the number of subarrays of length 3 such that the sum of
 # the first and third numbers equals exactly half of the second number.
 
+# here solution is - 
 
+class Solution(object):
+    def countSubarrays(self, nums):
+        n = len(nums)
+        ans = 0
+        for i in range(1, n - 1):
+            if nums[i] == (nums[i - 1] + nums[i + 1]) * 2:
+                ans += 1
+        return ans
 
  
