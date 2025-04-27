33e3c6f35e74d29d862cc35c4e13d87dd6ddd639
Abhay Mishra <techabhay06@gmail.com>
2025-04-27 09:22:52 +0530

    Add new commit

--- leetcode_ds_problem11.py
+++ leetcode_ds_problem11.py
@@ -1,5 +1,5 @@
 
-# here we solve leed code problem - 
+# here we solve leed code (Easy) problem - 
 
 # Given an integer array nums, return the number of subarrays of length 3 such that the sum of
 # the first and third numbers equals exactly half of the second number.
@@ -14,5 +14,3 @@ class Solution(object):
             if nums[i] == (nums[i - 1] + nums[i + 1]) * 2:
                 ans += 1
         return ans
-
- 
