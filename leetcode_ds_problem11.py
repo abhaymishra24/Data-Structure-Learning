1cc6eae38671d530dc13ee44a5323dd464fe0e79
Abhay Mishra <techabhay06@gmail.com>
2025-04-27 14:48:37 +0530

    Add new commit

--- leetcode_ds_problem11.py
+++ leetcode_ds_problem11.py
@@ -14,3 +14,7 @@ class Solution(object):
             if nums[i] == (nums[i - 1] + nums[i + 1]) * 2:
                 ans += 1
         return ans
+    
+s = Solution()
+nums = [1, 2, 3, 4, 5]
+print(s.countSubarrays(nums))  # Output: 0
