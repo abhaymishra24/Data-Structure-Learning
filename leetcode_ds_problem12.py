f77147880370a419ee4a1acc62f8799c63ca86fe
Abhay Mishra <techabhay06@gmail.com>
2025-04-30 10:39:16 +0530

    Add new commit

--- leetcode_ds_problem12.py
+++ leetcode_ds_problem12.py
@@ -1,24 +1,15 @@
 
-def findnumber(nums):
-    n = len(nums)
-    count = 0
-    for num in nums:
-        if len(str(abs(num))) % 2 == 0:
-            count += 1
-    return count
-
-f = findnumber()
-nums = [12, 345, 2, 6, 7896]
-print(f.findnumber(nums))  # Output: 2 (12 and 7896 have an even number of digits)
+# LeetCode Problem 1295: Find Numbers with Even Number of Digits:
+# Given an array nums of integers, return how many of them contain an even number of digits.
 
 
 def findNumbers(nums):
         n = len(nums)
         count = 0
-
         for n in nums:
 
             if len(str(abs(n))) % 2 == 0:
                 count += 1
 
         return count
+
