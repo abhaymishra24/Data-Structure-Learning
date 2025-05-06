7e64835929e0e4aa8846df32677c1d51f95e3901
Abhay Mishra <techabhay06@gmail.com>
2025-05-06 11:41:44 +0530

    Add new commit

--- leetcode_ds_problem12.py
+++ leetcode_ds_problem12.py
@@ -1,31 +1,31 @@
 
-# # LeetCode Problem 1295: Find Numbers with Even Number of Digits:
-# # Given an array nums of integers, return how many of them contain an even number of digits.
+# LeetCode Problem 1295: Find Numbers with Even Number of Digits:
+# Given an array nums of integers, return how many of them contain an even number of digits.
 
-# class Solution(object):
-#     def findNumbers(self, nums):
-#         count = 0
-#         for num in nums:
-#             if len(str(num)) % 2 == 0:
-#                 count += 1
-#         return count
+class Solution(object):
+    def findNumbers(self, nums):
+        count = 0
+        for num in nums:
+            if len(str(num)) % 2 == 0:
+                count += 1
+        return count
 
-# s = Solution()
-# nums = [34, 444, 22, 890, 11, 2]
-# print(s.findNumbers(nums))  # Output: 2 (12 and 7896 have an even number of digits) 
+s = Solution()
+nums = [34, 444, 22, 890, 11, 2]
+print(s.findNumbers(nums))  # Output: 2 (12 and 7896 have an even number of digits) 
 
 
-# class Solution(object):
-#     def findNumbers(self, nums):
-#         count = 0
-#         for num in nums:
-#             if len(str(num)) % 2 == 0:
-#                 count += 1
-#         return count
+class Solution(object):
+    def findNumbers(self, nums):
+        count = 0
+        for num in nums:
+            if len(str(num)) % 2 == 0:
+                count += 1
+        return count
 
-# s = Solution()
-# nums = [34, 444, 22, 890, 11, 2]
-# print(s.findNumbers(nums))
+s = Solution()
+nums = [34, 444, 22, 890, 11, 2]
+print(s.findNumbers(nums))
 
 
 # LeetCode Problem: Build Array from Permutation
