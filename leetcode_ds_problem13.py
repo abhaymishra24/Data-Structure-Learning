e1a56a9f4415907b936a0bf0e696b82595f5bb28
Abhay Mishra <techabhay06@gmail.com>
2025-05-22 13:57:36 +0530

    Add new  commit

--- leetcode_ds_problem13.py
+++ leetcode_ds_problem13.py
@@ -1,6 +1,12 @@
 
 # solve find out angle of triangle -
 
-str = " My name is money. I power full in every moment."
+# str = " My name is money. I power full in every moment."
 
-print(str.index("m"))
+# print(str.index("m"))
+
+a = 5
+b = 12
+c = 15
+
+print(max(a,b,c))
