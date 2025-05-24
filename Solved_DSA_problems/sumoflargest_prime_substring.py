28f02344a18491e59696091c18480cb3c2372059
Abhay Mishra <techabhay06@gmail.com>
2025-05-24 22:33:33 +0530

    Add new commit

---
+++ Solved_DSA_problems/sumoflargest_prime_substring.py
@@ -0,0 +1,26 @@
+
+class Solution(object):    
+    def sumOfLargestPrimes(self, s):
+        def is_prime(n):
+            if n < 2:
+                return False
+            if n == 2:
+                return True
+            if n % 2 == 0:
+                return False
+            for i in range(3, int(n**0.5) + 1, 2):
+                if n % i == 0:
+                    return False
+            return True
+
+        unique_primes = set()
+        n = len(s)
+        for i in range(n):
+            for j in range(i + 1, n + 1):
+                num_str = s[i:j]
+                if num_str.isdigit():
+                    num = int(num_str)
+                    if is_prime(num):
+                        unique_primes.add(num)
+        largest_primes = sorted(unique_primes, reverse=True)[:3]
+        return sum(largest_primes)
