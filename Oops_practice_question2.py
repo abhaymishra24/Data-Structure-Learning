598f5c4fd95c9b99542119697f41772e8ec8c7cb
Abhay Mishra <techabhay06@gmail.com>
2025-05-28 14:43:59 +0530

    Add new commit

--- Oops_practice_question2.py
+++ Oops_practice_question2.py
@@ -1,87 +1,87 @@
 
-# # Here solving basic to advance question on Oops concepts-
+# Here solving basic to advance question on Oops concepts-
 
-# # Start from here
+# Start from here
 
-# # Question 1 on payment transction voic notes 
+# Question 1 on payment transction voic notes 
 
-# class Payment:
+class Payment:
 
-#     def __init__(self,ammount):
-#         self.ammount=ammount
+    def __init__(self,ammount):
+        self.ammount=ammount
 
-#     def pay_money(self):
-#         print(f"Rupees {self.ammount} is pay on phonepay")
+    def pay_money(self):
+        print(f"Rupees {self.ammount} is pay on phonepay")
 
-#     def greet(self):
-#         print("Thankyou!")
+    def greet(self):
+        print("Thankyou!")
 
-# p=Payment(50)
-# p.pay_money()
-# p.greet()
+p=Payment(50)
+p.pay_money()
+p.greet()
 
-# # here very basic question-
+# here very basic question-
 
-# class number:
-#     def a_number(self, l1, l2):
+class number:
+    def a_number(self, l1, l2):
 
-#         self.l1=l1
-#         self.l2=l2
+        self.l1=l1
+        self.l2=l2
 
-#     def add(self):
-#         print(f"sum is {self.l1+self.l2}")
+    def add(self):
+        print(f"sum is {self.l1+self.l2}")
 
-# n=number()
-# n.a_number(6,7)
-# n.add()
+n=number()
+n.a_number(6,7)
+n.add()
 
-# # Here solve another oops concepts questions - 
+# Here solve another oops concepts questions - 
 
-# class City:
+class City:
 
-#     def __init__(self, area,toarea, Number):
-#         self.area=area
-#         self.toarea=toarea
-#         self.Number=Number
+    def __init__(self, area,toarea, Number):
+        self.area=area
+        self.toarea=toarea
+        self.Number=Number
 
-#     def bus_time(self,n):
-#         print(f"this {self.Number} bus from {self.area} to {self.toarea} at {n} will go")
-#         print(f"Number = {self.Number}")
+    def bus_time(self,n):
+        print(f"this {self.Number} bus from {self.area} to {self.toarea} at {n} will go")
+        print(f"Number = {self.Number}")
 
     
-# c = City("Ashok Nager", "Indira Nager", "224")
-# c.bus_time(f"{4}pm")   
+c = City("Ashok Nager", "Indira Nager", "224")
+c.bus_time(f"{4}pm")   
 
-# # here we solve calculation on two number -
+# here we solve calculation on two number -
 
-# def sum(n,m):
+def sum(n,m):
     
-#     print(f"sum is :{n+m}")
-#     print(f"sub is :{n-m}")
-#     print(f"divide is :{n//m}")
-#     print(f"multi is :{n*m}")
+    print(f"sum is :{n+m}")
+    print(f"sub is :{n-m}")
+    print(f"divide is :{n//m}")
+    print(f"multi is :{n*m}")
     
-# sum(8,8)
+sum(8,8)
 
  
-#  # here basice time problem , once time please watch this question and thik about it !
+ # here basice time problem , once time please watch this question and thik about it !
  
-# from datetime import datetime, timedelta
+from datetime import datetime, timedelta
 
-# date1 = datetime(2023, 5, 1)
-# date2 = datetime(2023, 5, 15)
-# delta = date2 - date1
-# print(delta.days)
+date1 = datetime(2023, 5, 1)
+date2 = datetime(2023, 5, 15)
+delta = date2 - date1
+print(delta.days)
 
-# # Explaination of this code - 
+# Explaination of this code - 
 
-# # The code calculates the difference between two datetime objects using the timedelta class. 
-# # The result is stored in the 'delta' variable.
-# # The 'days' attribute of timedelta gives the number of days between the two dates.
-# # In this case, the difference between May 15 and May 1 is 14 days.
+# The code calculates the difference between two datetime objects using the timedelta class. 
+# The result is stored in the 'delta' variable.
+# The 'days' attribute of timedelta gives the number of days between the two dates.
+# In this case, the difference between May 15 and May 1 is 14 days.
 
 
-# wrtie to programme to check that the list is palindrome or not -
+# wrtie to programme to check that the list is palindrome or not -(# this to build game on this code - )
 
 def is_palindrome(lst):
     lst1 = lst.copy()
@@ -98,4 +98,3 @@ def is_palindrome(lst):
 # Example call to the function
 is_palindrome([1, 2, 3, 2, 1])
         
- 
