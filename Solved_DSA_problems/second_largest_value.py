300942d52d9628c706ccb0507bbefa19f58b21ad
Abhay Mishra <techabhay06@gmail.com>
2025-06-07 20:01:01 +0530

    ADD new commit

--- Solved_DSA_problems/second_largest_value.py
+++ Solved_DSA_problems/second_largest_value.py
@@ -1,75 +1,75 @@
 
-# # here we solve second largest value - 
+# here we solve second largest value - 
 
-# # find out second largest number using sorting array
+# find out second largest number using sorting array
 
-# def second_largest_sorting(numbers):
-#     numbers.sort(reverse=True)
-#     return numbers[1]
+def second_largest_sorting(numbers):
+    numbers.sort(reverse=True)
+    return numbers[1]
 
-# numbers = [12, 45, 2, 41, 31, 10, 45, 65, 78]
-# second_largest = second_largest_sorting(numbers)
-# print("Second largest number:", second_largest)
+numbers = [12, 45, 2, 41, 31, 10, 45, 65, 78]
+second_largest = second_largest_sorting(numbers)
+print("Second largest number:", second_largest)
 
-# # same question solve with comments - 
+# same question solve with comments - 
 
-# def second_largest_sorting(numbers):
-#     # Sort the list of numbers in descending order (largest to smallest)
-#     numbers.sort(reverse=True)
+def second_largest_sorting(numbers):
+    # Sort the list of numbers in descending order (largest to smallest)
+    numbers.sort(reverse=True)
     
-#     # Return the second element in the sorted list, which is the second largest number
-#     return numbers[1]
+    # Return the second element in the sorted list, which is the second largest number
+    return numbers[1]
 
-# # Example list of numbers
-# numbers = [12, 45, 2, 41, 31, 10, 45, 65, 78]
+# Example list of numbers
+numbers = [12, 45, 2, 41, 31, 10, 45, 65, 78]
 
-# # Call the function to find the second largest number
-# second_largest = second_largest_sorting(numbers)
+# Call the function to find the second largest number
+second_largest = second_largest_sorting(numbers)
 
-# # Print the result
-# print("Second largest number:", second_largest)
+# Print the result
+print("Second largest number:", second_largest)
 
-# # solve once again - 
+# solve once again - 
 
-# def second_largest_sorting(numbers):
-#     numbers.sort(reverse=True)
-#     return numbers[1]
+def second_largest_sorting(numbers):
+    numbers.sort(reverse=True)
+    return numbers[1]
 
-# numbers = [12, 45, 2, 41, 31, 10, 45, 65, 78]
-# second_largest = second_largest_sorting(numbers)
-# print("Second largest number:", second_largest)
+numbers = [12, 45, 2, 41, 31, 10, 45, 65, 78]
+second_largest = second_largest_sorting(numbers)
+print("Second largest number:", second_largest)
 
-# # solve again with same method - 
+# solve again with same method - 
 
-# def second_large_valu(num):
+def second_large_valu(num):
     
-#     num.sort(reverse=True)
-#     return num[1]
+    num.sort(reverse=True)
+    return num[1]
 
-# num = [23,45,67,89]
-# second_value = second_large_valu(num)
-# print(f"the number is = {second_value}")
+num = [23,45,67,89]
+second_value = second_large_valu(num)
+print(f"the number is = {second_value}")
 
-# # here solve once again - 
+# here solve once again - 
 
-# def second_large(n):
+def second_large(n):
     
-#     num.sort(reverse=True)
-#     return num[1]
+    num.sort(reverse=True)
+    return num[1]
 
-# num = [2,3,4,5,6,7]
-# second = second_large(num)
-# print(second)
+num = [2,3,4,5,6,7]
+second = second_large(num)
+print(second)
 
-# # solve again tis one - 
+# solve again tis one - 
 
-# def sec_large(n):
+def sec_large(n):
     
-#     n.sort(reverse=True)
-#     return n[1]
+    n.sort(reverse=True)
+    return n[1]
 
-# n = [3,4,5,8,9]
-# value = sec_large(n)
-# print(value)
+n = [3,4,5,8,9]
+value = sec_large(n)
+print(value)
 
 # practice again - 
