cc6ebbf5f6d24d8fb6ebc0a36576b23046a8a1e1
Abhay Mishra <techabhay06@gmail.com>
2025-06-07 19:59:25 +0530

    Add new commit

--- Solved_DSA_problems/second_largest_value.py
+++ Solved_DSA_problems/second_largest_value.py
@@ -82,12 +82,10 @@ def sum_calculate(nums, target):
                 for k in range(j+1, n):
                     if nums[i] + nums[j] + nums[k] == target:
                         print(nums[i], nums[j], nums[k])
+                        return True
                         
-
         return False
 
 nums = [1,2,3,4,5,6,7]
 target = 9
 sum_calculate(nums, target)
-
-
