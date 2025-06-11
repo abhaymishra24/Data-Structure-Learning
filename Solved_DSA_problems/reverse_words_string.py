b2d53812de5345191ccf361cbfbab3d883eb7c47
Abhay Mishra <techabhay06@gmail.com>
2025-06-11 11:28:04 +0530

    Add new commit

--- Solved_DSA_problems/reverse_words_string.py
+++ Solved_DSA_problems/reverse_words_string.py
@@ -1,54 +1,51 @@
 
-# # Reverse words in string - 
+# Reverse words in string - 
 
-# class Solution(object):
-#     def reverseWords(self, s):
-#         s.strip()
-#         words = s.split()
-#         return " ".join(reversed(words))
+class Solution(object):
+    def reverseWords(self, s):
+        s.strip()
+        words = s.split()
+        return " ".join(reversed(words))
 
-# # Reverse words in string 
+# Reverse words in string 
 
-# def reverse_words(input_string: str) -> str:
-#     # Split the input string into a list of words
-#     words = input_string.split()
+def reverse_words(input_string: str) -> str:
+    # Split the input string into a list of words
+    words = input_string.split()
     
-#     # Reverse the list of words
-#     reversed_words = words[::-1]
+    # Reverse the list of words
+    reversed_words = words[::-1]
     
-#     # Join the reversed list back into a string
-#     reversed_string = ' '.join(reversed_words)
+    # Join the reversed list back into a string
+    reversed_string = ' '.join(reversed_words)
     
-#     return reversed_string
+    return reversed_string
 
-# # Example usage
-# input_string = "Hello, welcome to my world."
-# reversed_string = reverse_words(input_string)
-# print(f"Original String: '{input_string}'")
-# print(f"Reversed Words String: '{reversed_string}'")
+# Example usage
+input_string = "Hello, welcome to my world."
+reversed_string = reverse_words(input_string)
+print(f"Original String: '{input_string}'")
+print(f"Reversed Words String: '{reversed_string}'")
 
-# # same code without comment - 
+# same code without comment - 
 
-# # Split the string into words.
-# # Reverse the list of words.
-# # Join the reversed list back into a string.
+# Split the string into words.
+# Reverse the list of words.
+# Join the reversed list back into a string.
 
-# def reverse_words(input_string: str) -> str:
+def reverse_words(input_string: str) -> str:
      
-#     words = input_string.split()
+    words = input_string.split()
     
-#     reversed_words = words[::-1]
+    reversed_words = words[::-1]
     
-#     reversed_string = ' '.join(reversed_words)
+    reversed_string = ' '.join(reversed_words)
     
-#     return reversed_string
+    return reversed_string
 
-# input_string = "Hello, welcome to my world."
-# reversed_string = reverse_words(input_string)
-# print(f"Original String: '{input_string}'")
-# print(f"Reversed Words String: '{reversed_string}'")
+input_string = "Hello, welcome to my world."
+reversed_string = reverse_words(input_string)
+print(f"Original String: '{input_string}'")
+print(f"Reversed Words String: '{reversed_string}'")
 
-# # practice again - 
-
-
-
+# practice again - 
