6d8985a9af5f6446ce02be74901610642fe20254
Abhay Mishra <techabhay06@gmail.com>
2025-06-22 11:30:25 +0530

    Add new commit

--- leetcode_ds_problem14.py
+++ leetcode_ds_problem14.py
@@ -6,3 +6,26 @@
 #     print(k)  
  
 # reverse_string()
+
+
+def cal_num(a,b):
+    
+    sum = a+b
+    print(sum)
+    
+cal_num(10, 40)
+
+def cal_num2(m,n):
+    
+    return m-n
+
+r =  cal_num2(33, 8)
+print(r)
+    
+def cal_num3():
+    
+    n = int(input())
+    return n*n
+    
+res = cal_num3()
+print(res)
