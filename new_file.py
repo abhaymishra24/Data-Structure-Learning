59261bce643d30bcdaf1006b0f9b76f7835e548a
Abhay Mishra <techabhay06@gmail.com>
2025-08-12 12:06:51 +0530

    Add new commit

--- new_file.py
+++ new_file.py
@@ -1,3 +1,16 @@
 
 print("Hello world, this new file.")
 
+
+def number():
+    k = int(input())
+    m = int(input())
+    
+    print(k*m)
+    
+number()
+print("This number is the multiplication.")
+
+
+
+
