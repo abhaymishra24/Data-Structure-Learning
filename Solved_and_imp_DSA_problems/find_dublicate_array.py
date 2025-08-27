fa5ed36fabc0821862fb45ddbe3b72a9fabb19bd
Abhay Mishra <techabhay06@gmail.com>
2025-08-27 18:56:32 +0530

    Add new commit

--- Solved_and_imp_DSA_problems/find_dublicate_array.py
+++ Solved_and_imp_DSA_problems/find_dublicate_array.py
@@ -27,5 +27,31 @@ num = [1, 3, 4, 2, 2]
 res = find_dublicate(num)
 print(res)
 
+3,1,3,4,2
+
     
 
+def find_dublicate(num):
+    
+    slow = num[0]
+    fast = num[0]
+    
+    while True:
+        slow = num[slow]
+        fast = num[num[fast]]
+        
+        if slow == fast:
+            break
+        
+    slow = num[0]
+    
+    while slow != fast:
+        slow = num[slow]
+        fast = num[fast]
+        
+    return slow
+
+
+num = [1, 3, 4, 2, 2]
+res = find_dublicate(num)
+print(res)
