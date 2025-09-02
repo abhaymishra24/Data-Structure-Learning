82e4b0a98ef07bca6a5427cb4b24c8f9ef664d20
Abhay Mishra <techabhay06@gmail.com>
2025-09-02 18:09:46 +0530

    Add new commit

--- Solved_and_imp_DSA_problems/Rotated_sorted_array.py
+++ Solved_and_imp_DSA_problems/Rotated_sorted_array.py
@@ -1,3 +1,26 @@
 
 # here we solve the problem of finding an element in a rotated sorted array.
 
+# we use binary search technique to solve this problem.
+
+def search(nums, target):
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+
+        if nums[left] <= nums[mid]:  # Left half is sorted
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        else:  # Right half is sorted
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+    return -1
